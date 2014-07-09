dd6334bbebabf9b2de073871cdf15538f418e90f
imakewebthings <caleb@imakewebthings.com>
2014-07-08 18:55:48 -0700

    Notification script for use in guides

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
 (function() {
-  var $footer, $header, $main, $window
+  var $footer, $header, $main, $window, $notification
 
   function initVariables() {
     $footer = $('footer')
     $header = $('header')
     $main = $('main')
     $window = $(window)
+    $notifications = $('.notifications')
   }
 
   function centerMain() {
     var taken = $header.outerHeight(true) + $footer.outerHeight(true)
     var availableHeight = $window.height() - $main.outerHeight() - taken
     $main.css('top', availableHeight > 0 ? availableHeight / 2 : 0)
+  }
+
+  function notify(text) {
+    var $notification = $('<li />').text(text).css({
+      left: 320
+    })
+    $notifications.append($notification)
+    $notification.animate({
+      left: 0
+    }, 300, function() {
+      $(this).delay(3000).animate({
+        left: 320
+      }, 200, function() {
+        $(this).slideUp(100, function() {
+          $(this).remove()
+        })
+      })
+    })
   }
 
   $(function() {
@@ -22,4 +41,6 @@
   $(window).on('resize load', function() {
     centerMain()
   })
+
+  window.notify = notify
 }())
